936164be7a1a4970dfb60f73afa189a1811d3914
Marcos Amaral <marcos.professordelphi@gmail.com>
2018-11-20 22:14:16 -0200

    manutencao conflito

--- src/main/java/com/estudo/spring/Application.java
+++ src/main/java/com/estudo/spring/Application.java
@@ -1,5 +1,4 @@
 package com.estudo.spring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
